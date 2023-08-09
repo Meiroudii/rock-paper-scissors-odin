b0f74d87d9cac6bcaf77c8823bc63c61da51e82e
meiroudii <joelllena123@gmail.com>
2023-08-09 21:43:18 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+
+}
+
+function playRound(playerSelection, computerSelection) {
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {}
+
+console.log("Js loaded");
